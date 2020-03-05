5b9f3ebe0be41abc64fe5aba48a2153629a9aef4
PersonofNote <jessicaleetaylormartin@gmail.com>
2020-03-05 09:41:03 -0800

    Set up progress bar and text indicator

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ $(document).ready(function(){
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
-      adaptiveHeight: true
     });
   });
 
+
+$('.slider-wrapper').append('<div class="progress-container"></div>');
+
+$('.progress-container').append('<div class="progress-bar"></div>');
+
+$('.progress-container').append('<div class="progress" ></div>');
+
+$('.progress-container').append('<span class="progress-text"> </span>')
+
+
+
+$('.slider-container').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $('.progress-text').text(`${currentSlide} / ${slick.slideCount}`);
+    
+});
