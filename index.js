640c44d9dc2419fe93ed4779eb8a7ed21323040d
PersonofNote <jessicaleetaylormartin@gmail.com>
2020-03-05 12:10:48 -0800

    Add images, refine styling, add dummy content because it looks weird without anything

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ $(document).ready(function(){
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 1500,
+      initialSlide: 1
     });
   });
 
 
 $('.slider-wrapper').append('<div class="progress-container"></div>');
 
-$('.progress-container').append('<div class="progress-bar"></div>');
+$('.progress-container').append('<div class="bar-container"></div>');
 
-$('.progress-container').append('<div class="progress" ></div>');
+$('.bar-container').append('<div class="progress-bar"></div>');
+
+$('.bar-container').append('<div class="progress" ></div>');
 
 $('.progress-container').append('<span class="progress-text"> </span>')
 
+$('.slider-container').on('init', function(event, slick, currentSlide){
+    var i = (currentSlide ? currentSlide:0 ) + 1;
+    $('.progress-text').text(`${i} / ${slick.slideCount}`);
+    $('.progress').width(`${(i/slick.slideCount) * 100}%`);  
+});
 
 
-$('.slider-container').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+$('.slider-container').on('beforeChange', function(event, slick, currentSlide){
     var i = (currentSlide ? currentSlide : 0) + 1;
-    $('.progress-text').text(`${currentSlide} / ${slick.slideCount}`);
-    $('.progress').width(`${(currentSlide/slick.slideCount) * 100}%`);    
+    $('.progress-text').text(`${i} / ${slick.slideCount}`);
+    $('.progress').width(`${(i/slick.slideCount) * 100}%`);    
 });
